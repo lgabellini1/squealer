b720bcaf6ca148fd1d7c60f2a37b1b0a37a47c5d
SIJ82 <62840930+SIJ82@users.noreply.github.com>
2023-09-29 09:37:20 +0200

    init 2

--- server/server.js
+++ server/server.js
@@ -2,13 +2,22 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
 
 // Connessione a MongoDB
-mongoose.connect('mongodb://localhost:27017/mearnDB', {
-  useNewUrlParser: true,
+mongoose.connect('mongodb://orazio2014:orazio2014@cluster0.nzk477j.mongodb.net/?retryWrites=true&w=majority', {
+  useNewUrlParser: false,
   useUnifiedTopology: true,
 });
+
+const db = mongoose.connection;
+db.on('error', (error) => {
+  console.error('Errore nella connessione a MongoDB:', error);
+});
+db.once('open', () => {
+  console.log('Connesso con successo a MongoDB!');
+});
+
 
 app.get('/', (req, res) => {
   res.send('Hello from Express.js!');
